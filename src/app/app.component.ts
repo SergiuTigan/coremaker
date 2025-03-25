8beee4acdd3185f280c0099e73891113344dcd00
SergiuTigan <sergiu@tigan.dev>
2025-03-25 16:28:14 +0200

    Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { inject } from '@vercel/analytics';
 
 @Component({
     selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
     standalone: false
 })
 export class AppComponent {
+  constructor() {
+    inject();
+  }
 }
